9031a52dfd48dce16e3ca3fd0590b7f5d3ce8c93
Henrique Chaves Duarte <ht3036995@hto.ifsp.edu.br>
2025-03-17 21:59:03 -0300

    menu

--- Main.java
+++ Main.java
@@ -5,24 +5,31 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ArvoreBinariaBusca contatos = new ArvoreBinariaBusca();
-		
-		System.out.println("Bem vindo ao nosso gerenciador de contatos");
-		System.out.println("Escolha a opção desejada");
-		int opc = sc.nextInt();
-	    try {
-			opc = sc.nextInt();				
-		} catch (Exception e) {
-			sc.next().charAt(0);
-			System.out.println("Carácter inválido, por favor, use um número.\nExcessão: " + e);
-		}
+		int opc = 0;
 
 		do {
+			System.out.println("Bem vindo ao nosso gerenciador de contatos");
+			System.out.println("Escolha a opção desejada: ");
+			System.out.println("Opção 1: Adicionar um novo contato");
+			System.out.println("Opção 2: Pesquisar por um contato pelo código");
+			System.out.println("Opção 3: Listar todos os contatos em ordem alfabética");
+			System.out.println("Opção 4: Remover um contato por código");
+			System.out.println("Opção 5: Importar CSV");
+			System.out.println("Opção 6: Exibir estatísticas do sistema: ");
+			System.out.println("Opção 7: Sair");
+			try {
+				opc = sc.nextInt();				
+			} catch (Exception e) {
+				sc.next().charAt(0);
+				System.out.println("Carácter inválido, por favor, use um número.\nExcessão: " + e);
+			} 
 			switch(opc){
 				case 1: 
-					System.out.println("Digite o novo contato");
+					System.out.println("Digite o nome do novo contato: ");
 					System.out.print("Nome: ");
+					sc.nextLine();
 					String nome = sc.nextLine();
-					System.out.println("\nDigite o telefone (xxxxxxxxx): ");
+					System.out.println("Digite o telefone (xxxxxxxxx): ");
 					long tell = sc.nextLong();
 					System.out.println("\nDigite o código do " + nome);
 					int cod = sc.nextInt();	
@@ -31,8 +38,8 @@ public class Main {
 						break;
 					}else{
 						contatos.inserir(cod, nome, tell);
+						break;
 					}
-				break;
 				case 2:
 					System.out.println("Por favor, insíra o código do contato que quer encontrar:\n > ");
 					cod = 0;
